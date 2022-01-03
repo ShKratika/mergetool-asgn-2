b1baf37b535e5b8d5d370c611d37d6132410ad1e
ShKratika <50525627+ShKratika@users.noreply.github.com>
2022-01-03 17:45:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,7 @@ public class HelloWorld{
 	public static void main(String args[])
 	{
 		system.out.println("Hi from feature 1 branch");
-		
-	}
-
-	public static void main(String args[]
-	{
 		system.out.println("Hello from new feature 2 branch");
+		
 	}
 }
